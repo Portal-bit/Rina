5a7254944d9e41bd8f6704ecedc86105e018ac50
Reputable Production <julioferdinan773@gmail.com>
2026-08-15 00:17:20 +0700

    Update script.js

--- script.js
+++ script.js
@@ -280,7 +280,7 @@ document.addEventListener('DOMContentLoaded', () => {
 const dailyVideos = [
     {
         date: "14 AGUSTUS 2026",
-        video: "video",
+        video: "videos/14-agustus-2026.mp4",
         title: "Kabar aku hari ini ❤️",
         caption: "Hari ini aku mau cerita sesuatu..."
     },
